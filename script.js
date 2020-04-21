0c342a5d13f128677c4ded7793cb913dd9f08b41
benaor <benjamingirard25@gmail.com>
2020-04-21 22:40:28 +0200

    recover time value

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-let inputTime = document.querySelector('#time').value;
-let time      = parseInt(inputTime);
+//time value
+let time;
 
+//function for Recover value in input and convert on number
+function recoverTime() {
+    return time = parseInt(document.querySelector('#time').value);
+}
+//recover button 
+let button = document.querySelector('button');
+
+//add eventListener onclick button
+button.addEventListener('click', () => {
+    recoverTime();
+    console.log(time);
+})
