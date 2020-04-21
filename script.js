bdb07be497a0fa226841d8c9bc7f146ffbf09b88
benaor <benjamingirard25@gmail.com>
2020-04-21 22:53:27 +0200

    Contrain positif number

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-//time value
+zoneText = document.getElementById('timer');
 let time;
 
 //function for Recover value in input and convert on number
 function recoverTime() {
     return time = parseInt(document.querySelector('#time').value);
 }
+
 //recover button 
 let button = document.querySelector('button');
 
 //add eventListener onclick button
 button.addEventListener('click', () => {
     recoverTime();
-    console.log(time);
+   console.log(time);
+
+    if (time > 0) {
+        setInterval(() => {
+            if(time > 0){
+                time--;
+                zoneText.innerText = time;
+            }
+        }, 1000);
+    } else {
+        alert('Vous devez entrer un nombre positif');
+    }
+    
 })
